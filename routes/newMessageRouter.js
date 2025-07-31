0b4c3378a53aa083e19871ae0354b3e0b77a47c1
Adrian-Koh <adriankoh216@yahoo.com>
2025-08-01 02:13:30 +0800

    Add message to messages on submit button click
    Move messages array into individual file

--- routes/newMessageRouter.js
+++ routes/newMessageRouter.js
@@ -1,11 +1,14 @@
 const { Router } = require("express");
-
 const newMessageRouter = Router();
+const messages = require("../messages");
+
 newMessageRouter.get("/", (req, res) => {
   res.render("form");
 });
 
 newMessageRouter.post("/", (req, res) => {
+  const { user, text } = req.body;
+  messages.push({ user, text, added: new Date() });
   res.redirect("/");
 });
 
