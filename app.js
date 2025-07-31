d1a4002733e1bbbdb96a6731d8b598574830c5db
Adrian-Koh <adriankoh216@yahoo.com>
2025-08-01 00:21:40 +0800

    Add routes for index and new message form pages

--- app.js
+++ app.js
@@ -2,8 +2,14 @@ const path = require("node:path");
 const express = require("express");
 const app = express();
 
+const indexRouter = require("./routes/indexRouter");
+const newMessageRouter = require("./routes/newMessageRouter");
+
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
+
+app.use("/", indexRouter);
+app.use("/new", newMessageRouter);
 
 const PORT = 8000;
 app.listen(PORT, () => console.log(`Listening on port ${PORT}.`));
