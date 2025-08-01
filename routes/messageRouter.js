79196b12b56058f077a0fd16f5e253d12a7f4250
Adrian-Koh <adriankoh216@yahoo.com>
2025-08-01 13:25:03 +0800

    Update message page to include navbar

--- routes/messageRouter.js
+++ routes/messageRouter.js
@@ -1,10 +1,11 @@
 const { Router } = require("express");
 const messageRouter = Router();
 const messages = require("../messages");
+const links = require("../links");
 
 messageRouter.get("/:messageId", (req, res) => {
   const { messageId } = req.params;
-  res.render("message", messages[messageId]);
+  res.render("message", { ...messages[messageId], links });
 });
 
 module.exports = messageRouter;
